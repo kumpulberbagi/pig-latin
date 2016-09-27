68c4bec55c244ffc1f7bb8f4d158317443797f8c
Izhharuddin Syanmil <email": null>
2016-09-27 15:55:44 +0700

    1 release pig

--- pig_latin.js
+++ pig_latin.js
@@ -2,3 +2,29 @@
 
 //use readline to fix this challenge
 const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.setPrompt("Masukkan Kata Disini?");
+rl.prompt();
+rl.on('line', (input) => {
+  console.log(conPigLatin(input));
+});
+var conPigLatin = function (word) {
+  word = word.toLowerCase();
+  word = word.split("");
+  var vowel = ["a", "i", "u", "e", "o"];
+  var firstLetter = word.slice(0, 1);
+  if (firstLetter == "a" || firstLetter == "i" || firstLetter == "u" || firstLetter == "e" || firstLetter == "o") {
+    word = word.join("");
+    console.log(word)
+    return word;
+  } else {
+    word.splice(0, 1);
+    word = word.join("");
+    word = (word + firstLetter + "ay");
+    console.log(word)
+    return word
+  }
+}
