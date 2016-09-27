fc93a3ade11f0ddf5ad5b1b746ffc7a6c4f3556f
name": null, & Ken Duigraha Putra <pair+dharmadi93.kenduigraha@devbootcamp.com>
2016-09-27 15:32:18 +0700

    pig latin

--- pig_latin.js
+++ pig_latin.js
@@ -2,3 +2,52 @@
 
 //use readline to fix this challenge
 const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.setPrompt('Masukkan kata? ')
+rl.prompt();
+rl.on("line", (answer) => {
+  // TODO: Log the answer in a database
+
+  var kata=[];
+  var count=0;
+  kata = answer.split(" ");
+  if(kata.length == 1) {
+    single(answer,0); //0 buat kata
+  }
+  else {
+    var kalimat = [];
+    kalimat = kata;
+    for(var i = 0;i<kalimat.length; i++){
+      // console.log("kalimat split: " +kalimat[i]);
+      single(kalimat[i],1);
+    }
+  }
+
+  function single(answer, flag) {
+
+    kata = answer.split("");
+    // console.log("kalimat split: " +kata);
+    if(kata[0] == 'a' || kata[0] == 'i' || kata[0] == 'u' || kata[0] == 'e' || kata[0] == 'o') {
+      // console.log('kata: ', kata);
+    }
+    else {
+      var temp= kata[0];
+      kata.shift();
+      if(flag == 0) {
+        console.log(kata.join("") + temp[0] + "ay");
+      }
+      else {
+        console.log(kata.join("") + temp[0] + "ay");
+        count++;
+      }
+    }
+  }
+  console.log("Jumlah kata yang di convert: " + count);
+
+  rl.close();
+});
